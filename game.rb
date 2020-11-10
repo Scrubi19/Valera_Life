17e4ddc827b3b8873615f2267eea9179fb87acb4
scrubi19 <scrubi.memo@yandex.ru>
2020-11-10 22:55:17 +0700

    add main menu

--- game.rb
+++ game.rb
@@ -9,13 +9,16 @@ class Game
   end
 
   def create_session
+    Menu.print_main_menu
+    main_choice = Menu.select_in_range(3)
+    @valera = Modification.next_iteration(@valera, '9') if main_choice == '2'
     system('reset')
 
-    until @valera.stats!['state?']['dead']
+    until @valera.stats!['state?']['dead'] && main_choice == '1'
       Menu.print_stats(@valera.stats!)
       Menu.print_menu(@valera.stats!)
 
-      choice = Menu.select_in_range(10)
+      choice = Menu.select_in_range(9)
 
       @valera = Modification.next_iteration(@valera, choice)
       system('reset')
