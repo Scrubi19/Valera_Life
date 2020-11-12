6a2cb6f72d04a671b402368694c02e13ce566877
eiire <bulax.d.2101@gmail.com>
2020-11-12 16:16:27 +0700

    feature add yaml config for actions

--- lib/action/work.rb
+++ lib/action/work.rb
@@ -1,4 +1,5 @@
 require_relative 'action_mixins/change_stats_mixin'
+require 'yaml'
 
 class Work
   include ChangeStatsMixin
@@ -6,15 +7,20 @@ class Work
 
   def initialize(base_object)
     @base_object = base_object
+    stats_config = YAML.safe_load(File.read('lib/action_config.yml'))['work']
+    @fun = stats_config['fun']
+    @mana = stats_config['mana']
+    @fatigue = stats_config['fatigue']
+    @money = stats_config['money']
   end
 
   def stats!
     @stats = @base_object.stats!.clone
 
-    @stats['fun'] = take_op 'fun', '-', 5
-    @stats['mana'] = take_op 'mana', '-', 30
-    @stats['money'] = take_op 'money', '+', 100
-    @stats['fatigue'] = take_op 'fatigue', '+', 70
+    @stats['fun'] = take_op 'fun', '-', @fun
+    @stats['mana'] = take_op 'mana', '-', @mana
+    @stats['money'] = take_op 'money', '+', @money
+    @stats['fatigue'] = take_op 'fatigue', '+', @fatigue
 
     @stats
   end
